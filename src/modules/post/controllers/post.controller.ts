5b1d67cde061aa27dee808e6c7940aa6656519e8
Moein <moein.azizi1997@gmail.com>
2026-08-03 01:55:57 +0330

    Implemented create post logic

--- src/modules/post/controllers/post.controller.ts
+++ src/modules/post/controllers/post.controller.ts
@@ -1,7 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { asyncHandler } from "../../../middlewares/asyncHandler";
 import PostService from "../services/post.service";
+import { HTTPSTATUS } from "../../../config/http.config";
+import { ApiResponse } from "../../../utils/apiResponse";
 
 class PostController{
     constructor(public readonly postService : PostService){}
+
+    public createPost = asyncHandler(
+        async (req:any,res:Response,next:NextFunction): Promise<Response>=>{
+            const imagePath = req.file ? `/uploads/${req.file.filename}` : undefined;
+            const post = await this.postService.createPost(req.user.id,req.body,imagePath);
+            return res.status(HTTPSTATUS.CREATED).json(ApiResponse.success(post));
+        }
+    );
 };
 
 export default PostController;
--- src/modules/post/services/post.service.ts
+++ src/modules/post/services/post.service.ts
@@ -1,5 +1,29 @@
-class PostService{
+import { BadRequestException, NotFoundException } from "../../../utils/catch-error";
+import logger from "../../../utils/logger";
+import { User } from "../../user/models/user.model";
+import { CreatePostDTO } from "../DTOs/post.dto";
+import { IPost,Post } from "../models/post.model";
 
+class PostService{
+    public async createPost(authorId : string, dto: CreatePostDTO, imagePath?: string): Promise<IPost>{
+        const existingUser = await User.findOne({ _id: authorId });
+
+        if (existingUser) {
+            throw new NotFoundException("User with this authorId not found.");
+        }
+
+        const post = await Post.create({
+            ...dto,
+            image: imagePath || dto.image,
+            author: authorId
+        });
+
+        logger.info(`User created: ${post.title}`);
+
+        return post;
+    };
+
+    
 };
 
 export default PostService;
